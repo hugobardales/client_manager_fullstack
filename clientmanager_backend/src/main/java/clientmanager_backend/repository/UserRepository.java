360f3c39a6387b835205d3b7166f2f7a00ea6e3a
hugobardales <vhbardalesp@gmail.com>
2026-02-19 13:01:30 -0500

    feat: create UserRepositoryClass to manage the communication with the db.

---
+++ clientmanager_backend/src/main/java/clientmanager_backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package clientmanager_backend.repository;
+
+import clientmanager_backend.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
